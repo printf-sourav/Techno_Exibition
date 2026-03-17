9b30c8308ae66c4db38fb764f2b8dcaf0b75f813
lishhh06 <lishikameghani@gmail.com>
2026-03-18 02:14:55 +0530

    final utils

--- Backend/utils/ApiError.js
+++ Backend/utils/ApiError.js
@@ -1,8 +1,15 @@
 class ApiError extends Error {
-  constructor(statusCode, message) {
+  constructor(statusCode, message="smthing went wrong", errors = [], stack = "") {
     super(message);
     this.statusCode = statusCode;
+    this.success = false;
+    this.message=message;
+    this.errors = errors;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
   }
 }
-
 export default ApiError;
