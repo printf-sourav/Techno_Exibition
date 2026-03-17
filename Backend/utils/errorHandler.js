ad24b9b1f0933b1bb651c32c960f7cfac6afd1e8
lishhh06 <lishikameghani@gmail.com>
2026-03-18 02:04:47 +0530

    utils apierr,res

--- Backend/utils/errorHandler.js
+++ Backend/utils/errorHandler.js
@@ -1,7 +1,7 @@
 
 const errorHandler=(err,req,res,next)=>{
     console.error(err.stack);
-    const statusCode=err.statusCode || 500;
+   const statusCode = err.statusCode || 500;
     res.status(statusCode).json({
         success:false,
         message:err.message || "Server Error"
