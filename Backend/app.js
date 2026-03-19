063866f1a0a947494427f8479c599008117eb88a
printf-sourav <lcs.souravkrsahu@gmail.com>
2026-03-19 16:23:27 +0530

    App.js updated

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("API running");
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route not found"
+  });
+});
+
 import authRoutes from "./src/routes/auth.routes.js"
 
 app.use("/api/auth",authRoutes)
